9441f18cea63753ed8a96fbd1d0d6d3e1d757fa3
Khoi Nguyen <khnaero@gmail.com>
2017-05-05 15:16:16 -0500

    Add Paperclip image upload

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ActiveRecord::Base
   belongs_to :user
+
+  # Use '#' instead of '>' to crop image
+  has_attached_file :image, styles: { medium: "400x600#" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
